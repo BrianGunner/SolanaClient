e014c4e09ebe081287c2c73bb0d82b726fadc5b6
BrianGunner <bhargav@avalialearning.com>
2026-06-25 21:18:52 +0530

    Mint Initialized

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use solana_program::program_pack::Pack;
 use std::any;
 use std::str::FromStr;
 use spl_token::state::Mint;
+use std::fs;
+use solana_sdk::signature::write_keypair_file;
 
 
 
@@ -22,50 +24,52 @@ fn main()->Result<()>{
     let main_account = read_keypair_file("id.json").map_err(|e|anyhow::anyhow!("Could not load keypair: {}",e))?;
     let main_pubkey = main_account.pubkey();
 
-    let solomon_token = Keypair::new();
+    let solomon_token = read_keypair_file("solomon-mint.json").map_err(|e|anyhow::anyhow!("Could not load keypair: {}",e))?;
     let solomon_pubkey = solomon_token.pubkey();
 
-    let space = Mint::LEN;
-    let space_lamports = client.get_minimum_balance_for_rent_exemption(space)?;
-    let recent_blockhash = client.get_latest_blockhash()?;
+    let token_account = read_keypair_file("token-account.json").map_err(|e|anyhow::anyhow!("could not load token-account: {}",e))?;
+    let token_pubkey = token_account.pubkey();
 
-    let acc_instruction = system_instruction::create_account(
+    let space = TokenAccount::LEN;
+    let space_lamports = client.get_minimum_balance_for_rent_exemption(space)?;
+
+    let recent_blockhash = client.get_latest_blockhash()?;
+    
+    let token_account_instruction = system_instruction::create_account(
         &main_pubkey, 
-        &solomon_pubkey, 
+        &token_pubkey, 
         space_lamports, 
         space as u64, 
         &spl_token::ID,
     );
 
-    let mint_instruction = spl_token::instruction::initialize_mint(
+    let init_token_acc_instruction = spl_token::instruction::initialize_account(
         &spl_token::ID, 
+        &token_pubkey, 
         &solomon_pubkey, 
-        &main_pubkey, 
-        Some(&main_pubkey), 
-        6,
+        &main_pubkey,
     )?;
 
-    let tx = Transaction::new_signed_with_payer(
-        &[acc_instruction,mint_instruction], 
+    let init_tx = Transaction::new_signed_with_payer(
+        &[token_account_instruction,init_token_acc_instruction], 
         Some(&main_pubkey), 
-        &[&main_account,&solomon_token], 
+        &[&main_account,&token_account],
         recent_blockhash,
     );
 
-    let signature = client.send_and_confirm_transaction(&tx);
+    let signature = client.send_and_confirm_transaction(&init_tx);
 
     match signature{
         std::result::Result::Ok(sig)=>println!("Solomon Token Mint created with signature: {}",sig),
         Err(msg)=>println!("{}",msg),
     }
-    println!("Solomon Token Pubkey: {}",solomon_pubkey);
-    let solomon_info = client.get_account(&solomon_pubkey)?;
-    
-    println!("Solomon Owner: {}",solomon_info.owner);
-    println!("Solomon lamports: {}",solomon_info.lamports);
-    println!("Solomon Executable: {}",solomon_info.executable);
-    println!("Solomon Data Length {}",solomon_info.data.len());
 
+    println!("Token Account Pubkey: {}",token_account.pubkey());
+    let token_account_info = client.get_account(&token_pubkey)?;
+    println!("Token Owner: {}",token_account_info.owner);
+    println!("Token lamports: {}",token_account_info.lamports);
+    println!("Token Executable: {}",token_account_info.executable);
+    println!("Token Data Length {}",token_account_info.data.len());
 
     
     Ok(())
